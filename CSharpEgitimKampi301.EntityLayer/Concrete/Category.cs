3faf61f5fb391dff65460d5cf23d880f248a5da8
Enes Akkaya <95829445+AkkayaEnes@users.noreply.github.com>
2025-10-15 22:30:56 +0300

    Add project files.

---
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Category.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEgitimKampi301.EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } //get almak set ayarlamak
+        public bool CategoryStatus { get; set; }
+    }
+}
+
+/*
+Field-Variable-Property
+ */
+
+//int x; //field
+//public int y { get; set; } //property
+
+//void test()
+//{
+//    int z; //değişken
+//}

2fbe93266ce5a83d91af45b62deaf99f5a8fe53b
Enes Akkaya <95829445+AkkayaEnes@users.noreply.github.com>
2025-10-23 22:24:54 +0300

    remake

--- CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
+++ CSharpEgitimKampi301.DataAccessLayer/Context/KampContext.cs
@@ -10,6 +10,7 @@ namespace CSharpEgitimKampi301.DataAccessLayer.Context
 {
     public class KampContext : DbContext
     {
+        
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
